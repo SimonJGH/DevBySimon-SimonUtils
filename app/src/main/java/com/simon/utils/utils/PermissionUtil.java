b796d04c729cbcf1e98a661b20b7bbad38b2250c
SimonJGH <simon_19900501@163.com>
2018-03-27 13:55:56 +0800

    修正权限工具

--- app/src/main/java/com/simon/utils/utils/PermissionUtil.java
+++ app/src/main/java/com/simon/utils/utils/PermissionUtil.java
@@ -1,5 +1,6 @@
 package com.simon.utils.utils;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -9,41 +10,223 @@ import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
+ * 权限请求工具类
  * Created by Simon on 2017/6/25.
  * <p>
- * PermissionUtil.requestPermission(HomeActivity.this, new String[]{
- * Manifest.permission.ACCESS_COARSE_LOCATION,
- * Manifest.permission.CAMERA,
- * Manifest.permission.WRITE_EXTERNAL_STORAGE,
- * Manifest.permission.READ_PHONE_STATE});
+ * mPermissionUtil = new PermissionUtil(MainActivity.this);
+ * mPermissionUtil.requertLocationPermission(0);
+ * <p>
+ * switch (requestCode) {
+ * case 0:
+ * boolean location = (grantResults[0] == PackageManager.PERMISSION_GRANTED);
+ * if (location) {
+ * // 已获取权限-todo
+ * } else {
+ * mPermissionUtil.requertLocationPermission(0);
+ * }
+ * break;
+ * }
  */
-
+@SuppressWarnings("all")
 public class PermissionUtil {
 
-    //默认请求码
-    public static final int REQUESTCODE = 110;
+    private Context mContext;
 
-    /**
-     * 检测权限-适用于单个功能模块
-     *
-     * @param context
-     * @param permission
-     */
-    public static void checkPermission(Context context, String permission) {
-        if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
-            judgePermission(context, permission);
-        }
+    public PermissionUtil(Context context) {
+        this.mContext = context;
     }
 
     /**
-     * 请求权限-适用于第一次app启动
-     *
-     * @param context
-     * @param permissions
+     * 请求获取拍照权限
+     * permission:android.permission.CAMERA
      */
-    public static void requestPermission(Context context, String[] permissions) {
-        ActivityCompat.requestPermissions((Activity) context, permissions, REQUESTCODE);
+    public boolean requestCameraPermission(int requestCode) {
+        boolean b = checkPermission(mContext, Manifest.permission.CAMERA, requestCode);
+        if (b) {
+            return true;
+        } else {
+            requestPermission(mContext, new String[]{Manifest.permission.CAMERA}, requestCode);
+        }
+        return false;
+    }
+
+    /**
+     * 请求获取录音权限
+     * permission:android.permission.RECORD_AUDIO
+     */
+    public boolean requestAudioPermission(int requestCode) {
+        boolean b = checkPermission(mContext, Manifest.permission.RECORD_AUDIO, requestCode);
+        if (b) {
+            return true;
+        } else {
+            requestPermission(mContext, new String[]{Manifest.permission.RECORD_AUDIO}, requestCode);
+        }
+        return false;
+    }
+
+    /**
+     * 请求获取写入数据权限
+     * permission:android.permission.READ_EXTERNAL_STORAGE
+     * permission:android.permission.WRITE_EXTERNAL_STORAGE
+     */
+    public boolean requestStoragePermission(int requestCode) {
+        boolean b = checkPermission(mContext, Manifest.permission.WRITE_EXTERNAL_STORAGE, requestCode);
+        if (b) {
+            return true;
+        } else {
+            requestPermission(mContext, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, requestCode);
+        }
+        return false;
+    }
+
+    /**
+     * 请求获取定位权限
+     * permission:android.permission.ACCESS_FINE_LOCATION
+     * permission:android.permission.ACCESS_COARSE_LOCATION
+     */
+    public boolean requestLocationPermission(int requestCode) {
+        boolean b = checkPermission(mContext, Manifest.permission.ACCESS_COARSE_LOCATION, requestCode);
+        if (b) {
+            return true;
+        } else {
+            requestPermission(mContext, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, requestCode);
+        }
+        return false;
+    }
+
+    /**
+     * 请求获取联系人权限
+     * permission:android.permission.WRITE_CONTACTS
+     * permission:android.permission.GET_ACCOUNTS
+     * permission:android.permission.READ_CONTACTS
+     */
+    public boolean requestContactPermission(int requestCode) {
+        boolean b = checkPermission(mContext, Manifest.permission.WRITE_CONTACTS, requestCode);
+        if (b) {
+            return true;
+        } else {
+            requestPermission(mContext, new String[]{Manifest.permission.WRITE_CONTACTS}, requestCode);
+        }
+        return false;
+    }
+
+    /**
+     * 请求获取短信权限
+     * permission:android.permission.READ_SMS
+     * permission:android.permission.RECEIVE_WAP_PUSH
+     * permission:android.permission.RECEIVE_MMS
+     * permission:android.permission.RECEIVE_SMS
+     * permission:android.permission.SEND_SMS
+     * permission:android.permission.READ_CELL_BROADCASTS
+     */
+    public boolean requestSmsPermission(int requestCode) {
+        boolean b = checkPermission(mContext, Manifest.permission.SEND_SMS, requestCode);
+        if (b) {
+            return true;
+        } else {
+            requestPermission(mContext, new String[]{Manifest.permission.SEND_SMS}, requestCode);
+        }
+        return false;
+    }
+
+    /**
+     * 请求获取手机状态权限
+     * permission:android.permission.READ_CALL_LOG
+     * permission:android.permission.READ_PHONE_STATE
+     * permission:android.permission.CALL_PHONE
+     * permission:android.permission.WRITE_CALL_LOG
+     * permission:android.permission.USE_SIP
+     * permission:android.permission.PROCESS_OUTGOING_CALLS
+     * permission:com.android.voicemail.permission.ADD_VOICEMAIL
+     */
+    public boolean requestPhoneStatePermission(int requestCode) {
+        boolean b = checkPermission(mContext, Manifest.permission.READ_PHONE_STATE, requestCode);
+        if (b) {
+            return true;
+        } else {
+            requestPermission(mContext, new String[]{Manifest.permission.READ_PHONE_STATE}, requestCode);
+        }
+        return false;
+    }
+
+    /**
+     * 请求获取日历权限
+     * permission:android.permission.READ_CALENDAR
+     * permission:android.permission.WRITE_CALENDAR
+     */
+    public boolean requestCalendarPermission(int requestCode) {
+        boolean b = checkPermission(mContext, Manifest.permission.WRITE_CALENDAR, requestCode);
+        if (b) {
+            return true;
+        } else {
+            requestPermission(mContext, new String[]{Manifest.permission.WRITE_CALENDAR}, requestCode);
+        }
+        return false;
+    }
+
+    /**
+     * 请求获取传感器权限
+     * permission:android.permission.BODY_SENSORS
+     */
+    public boolean requestSensorsPermission(int requestCode) {
+        boolean b = checkPermission(mContext, Manifest.permission.BODY_SENSORS, requestCode);
+        if (b) {
+            return true;
+        } else {
+            requestPermission(mContext, new String[]{Manifest.permission.BODY_SENSORS}, requestCode);
+        }
+        return false;
+    }
+
+    /**
+     * 请求获取多个权限 - 一般用于首次进入提示
+     * List<String> mPermissionsList = new ArrayList<>();
+     * mPermissionsList.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+     * mPermissionsList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+     * boolean b = requestPermissions(mPermissionsList);
+     */
+    public boolean requestPermissions(List<String> permissionsList) {
+        boolean flag = true;
+        List<String> mPermissionsList = new ArrayList<>();
+        if (!mPermissionsList.isEmpty()) {
+            mPermissionsList.clear();
+        }
+        for (int i = 0; i < permissionsList.size(); i++) {
+            String permission = permissionsList.get(i);
+            boolean b = checkPermission(mContext, permission, 10);
+            if (!b) {
+                flag = b;
+                mPermissionsList.add(permission);
+            }
+        }
+        if (!mPermissionsList.isEmpty()) {
+            String[] permissions = (String[]) mPermissionsList.toArray(new String[mPermissionsList.size()]);
+            requestPermission(mContext, permissions, 10);
+        }
+        return flag;
+    }
+
+    /**
+     * 检测权限
+     */
+    private boolean checkPermission(Context context, String permission, int code) {
+        if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+            // judgePermission(context, permission, code);
+        } else {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 请求权限
+     */
+    private void requestPermission(Context context, String[] permissions, int code) {
+        ActivityCompat.requestPermissions((Activity) context, permissions, code);
     }
 
     /**
@@ -53,18 +236,18 @@ public class PermissionUtil {
      * -----------如果应用第一次请求权限或 用户在过去拒绝了权限请求，
      * -----------并在权限请求系统对话框中选择了 Don't ask again 选项，此方法将返回 false。
      */
-    private static void judgePermission(Context context, String permission) {
+    private void judgePermission(Context context, String permission, int code) {
         if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context, permission)) {
-            requestPermission(context, new String[]{permission});
+            requestPermission(context, new String[]{permission}, code);
         } else {
-            toAppSetting(context);
+            toPermissionSetting(context);
         }
     }
 
     /**
      * 跳转到权限设置界面
      */
-    private static void toAppSetting(Context context) {
+    private void toPermissionSetting(Context context) {
         Intent intent = new Intent();
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         if (Build.VERSION.SDK_INT >= 9) {
